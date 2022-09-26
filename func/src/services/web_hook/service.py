cd214349bae63e41d9bfca105d1f2b5f9ea8d14f
Marco Sievers de Almeida <msa@lionx.com.br>
2022-09-26 07:10:44 -0300

    feat: save payment order on data base

--- func/src/services/web_hook/service.py
+++ func/src/services/web_hook/service.py
@@ -1,12 +1,14 @@
 # PROJECT IMPORTS
 from src.domain.validator.webhook.validator import WebHookMessage
 from src.trasnport.bifrost import BifrostTransport
+from src.repositories.payment_order.repository import PaymentOrderRepository
 
 
 class ForexAccountService:
 
     @classmethod
     async def request_payment_order_process(cls, webhook_message: WebHookMessage) -> bool:
+        await PaymentOrderRepository.save_payment_order(webhook_message=webhook_message)
         result = await BifrostTransport.send_payment_order_to_process(
             webhook_message=webhook_message
         )
